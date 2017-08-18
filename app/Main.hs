be3a7ad40a451f8ea1277e414e6011718e611692
jali-clarke <jinnah.ali-clarke@outlook.com>
2017-08-18 00:46:36 -0400

    created colour map

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,21 @@
 module Main where
 
+import System.Environment(getArgs)
+import System.Console.Terminal.Size
+
 import PicConvert
 
 main :: IO ()
-main = someFunc
+main = do
+    args <- getArgs
+    if null args
+        then putStrLn "no filename given"
+        else do
+            result <- fetchImage (head args)
+            case result of
+                Left err -> putStrLn err
+                Right img -> do
+                    maybeSize <- size
+                    case maybeSize of
+                        Nothing -> putStrLn "could not get terminal size"
+                        Just (Window _ width) -> putStrLn "" >> renderImage (scaleImage width img) >> putStrLn ""
--- src/PicConvert.hs
+++ src/PicConvert.hs
@@ -1,14 +1,54 @@
 module PicConvert (
     fetchImage,
+    scaleImage,
     renderImage
 ) where
 
-import Codec.Picture(convertRGB8, readImage, Image(..), PixelRGB8(..))
+import Codec.Picture
 import System.Console.ANSI
+import Data.List (minimumBy)
 
--- give it the image path and return either an error or the image data (squashed)
+-- give it the image path and return either an error or the image data (colours squashed)
 fetchImage :: FilePath -> IO (Either String (Image PixelRGB8))
 fetchImage = fmap (fmap convertRGB8) . readImage
 
--- takes output length and width, followed by the image data
-renderImage :: Int -> Int -> Image PixelRGB8 -> IO ()
+-- new width -> image data -> scaled image
+scaleImage :: Int -> Image PixelRGB8 -> Image PixelRGB8
+scaleImage = undefined
+
+colourMap :: [((Int, Int, Int), (Color, ColorIntensity))]
+colourMap = [
+    ((0, 0, 0), (Black, Dull)), -- black
+    ((128, 0, 0), (Red, Dull)), -- red
+    ((0, 128, 0), (Green, Dull)), -- green
+    ((128, 128, 0), (Yellow, Dull)), -- yellow
+    ((0, 0, 128), (Blue, Dull)), -- blue
+    ((128, 0, 128), (Magenta, Dull)), -- magenta
+    ((0, 128, 128), (Cyan, Dull)), -- cyan
+    ((192, 192, 192), (White, Dull)), -- light gray
+    ((128, 128, 128), (Black, Vivid)), -- dark gray
+    ((255, 0, 0), (Red, Vivid)), -- light red
+    ((0, 255, 0), (Green, Vivid)), -- light green
+    ((255, 255, 0), (Yellow, Vivid)), -- light yellow
+    ((0, 0, 255), (Blue, Vivid)), -- light blue
+    ((255, 0, 255), (Magenta, Vivid)), -- light magenta
+    ((0, 255, 255), (Cyan, Vivid)), -- light cyan
+    ((255, 255, 255), (White, Vivid))-- white
+    ]
+
+-- convert RGB8 to console colours
+colourConvert :: PixelRGB8 -> (Color, ColorIntensity)
+colourConvert (PixelRGB8 r g b) = snd $ minimumBy minFunc colourMap
+    where
+        rInt = fromIntegral r
+        gInt = fromIntegral g
+        bInt = fromIntegral b
+
+        dist (cr, cg, cb) = abs (cr - rInt) + abs (cg - gInt) + abs (cb - bInt)
+
+        minFunc (c0, _) (c1, _) = dist c0 `compare` dist c1
+
+-- takes image data to render, and renders it
+renderImage :: Image PixelRGB8 -> IO ()
+renderImage img@(Image imgWidth imgHeight _) = do
+    putStrLn ""
