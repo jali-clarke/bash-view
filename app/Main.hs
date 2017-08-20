612665a9fe6c2b1b5f05d94228d129c712cd00b0
jali-clarke <jinnah.ali-clarke@outlook.com>
2017-08-20 13:45:26 -0400

    fixed pyramid of death

--- app/Main.hs
+++ app/Main.hs
@@ -2,21 +2,25 @@ module Main where
 
 import System.Environment (getArgs)
 import System.Console.Terminal.Size (Window(..), size)
+import Control.Monad.Except
+import Control.Monad (when)
 
 import PicConvert
 
--- could have used a monad stack here but the app is simple enough that it isn't necessary
 main :: IO ()
 main = do
-    args <- getArgs
-    if null args
-        then putStrLn "no filename given"
-        else do
-            result <- fetchImage (head args)
-            case result of
-                Left err -> putStrLn err
-                Right img -> do
-                    maybeSize <- size
-                    case maybeSize of
-                        Nothing -> putStrLn "could not get terminal size"
-                        Just (Window _ width) -> putChar '\n' >> renderImage (scaleImage width img) >> putChar '\n'
+    actionResult <- runExceptT $ do
+        args <- liftIO getArgs
+        when (null args) (throwError "no filename given")
+
+        result <- liftIO (fetchImage (head args))
+        img <- ExceptT (return result)
+        maybeSize <- liftIO size
+        maybe (throwError "could not get terminal size") (liftIO . renderAction img) maybeSize
+
+    case actionResult of
+        Left err -> putStrLn err
+        Right _ -> return ()
+
+    where
+        renderAction img (Window _ width) = putChar '\n' >> renderImage (scaleImage width img) >> putChar '\n'
